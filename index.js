acd709d093e0423f55ebe5bc15677b0b118ee37a
nunjayi <epenyash@gmail.com>
2024-03-23 21:00:28 +0300

    using arithmetic operators, and built in objects: Math and Number

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 =2
+let num2 =31
+let num3 = 44
+let num4 = 10
+
+let multiply = num1 * num2;
+
+
+let random = Math.ceil(Math.random())
+    console.log(Number.isInteger(random))
+
+let mod = num3 % num4
+let max = Math.max(1,2,4,10,20)
